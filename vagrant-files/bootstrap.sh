32ac88e6c22831719f0863c549d180fcc7c73a96
NaGeL182 <nagel182@gmail.com>
2018-01-29 19:47:53 +0100

    working vagrant

---
+++ vagrant-files/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository -y ppa:ondrej/php
+sudo add-apt-repository -y ppa:ondrej/apache2
+sudo apt-get update
+sudo apt-get install -y apache2 php5.6
+sudo a2enmod rewrite
+sudo /bin/cp -rf /vagrant/vagrant-files/000-default.conf /etc/apache2/sites-available/000-default.conf
+sudo service apache2 restart
+if ! [ -L /var/www/html ]; then
+    sudo rm -rf /var/www/html
+    sudo ln -fs /vagrant /var/www/html
+fi
